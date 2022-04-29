0ae4633dcc93552015e8336c850dea6d6b58e258
shubham pandav <shubhampandav1479@gmail.com>
2022-04-29 20:15:40 +0530

    comit

---
+++ app/src/main/java/com/example/whatsappclone/model/userModel.kt
@@ -0,0 +1,10 @@
+package com.example.whatsappclone.model
+
+class userModel {
+
+    val uid:String = ""
+    val name:String = ""
+    val mobile:String =""
+    val image:String = ""
+
+}
--- app/src/main/java/com/example/whatsappclone/profile.kt
+++ app/src/main/java/com/example/whatsappclone/profile.kt
@@ -1,34 +1,90 @@
 package com.example.whatsappclone
 
-import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.whatsappclone.R
+import com.example.whatsappclone.model.userModel
+import com.google.common.collect.Iterables
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import java.util.*
 
 class profile : AppCompatActivity() {
 
-    val userimge: ImageView? = null;
+    lateinit var image: ImageView;
+    lateinit var nextbnt: Button
+    lateinit var name: TextView;
+    private var pickImage = 100
+    private var imageUri: Uri? = null
+    lateinit var auth: FirebaseAuth;
+    lateinit var storage:FirebaseStorage;
 
-    private var image: Uri? = null
-    private  var REQUEST_CODE = 100;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        supportActionBar!!.hide(); // this is fro hide the action bar
-        val userimg = findViewById<ImageView>(R.id.userimg);
+        image = findViewById(R.id.image);
+        auth = FirebaseAuth.getInstance();
+        nextbnt = findViewById(R.id.nextbtn);
+        name = findViewById(R.id.name);
+        storage = FirebaseStorage.getInstance();
+
+        supportActionBar?.hide() // this is for hide the action bar
+        image.setOnClickListener {
+            val gallery =
+                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI).also {
+                    startActivityForResult(it, pickImage)
+                }
+
+        }
+        nextbnt.setOnClickListener {
+            if (name.text.toString().isEmpty()) {
+                Toast.makeText(this, "Enter your name", Toast.LENGTH_SHORT).show()
+            } else if (image == null) {
+                Toast.makeText(this, "select profile picture ", Toast.LENGTH_SHORT).show()
+            } else {
+                uploadData();
+            }
+
+        }
+
+    }
+
+    private fun uploadData() {
+        val reference = storage.reference.child("profile").child(Date().time.toString())
+        imageUri?.let {
+            reference.putFile(it).addOnCompleteListener() {
+                if(it.isSuccessful){
+                  reference.downloadUrl.addOnCompleteListener {
+                      uploadInfo(it.toString())
+                  }
+                }
+            }
+        }
+
+    }
+
+    private fun uploadInfo(imageUrl: String) {
+
 
 
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == pickImage) {
+            imageUri = data?.data
+            image.setImageURI(imageUri)
+
+        }
+    }
 }
-
-
-
-
-
 
 
 
